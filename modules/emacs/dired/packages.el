3bf088a328c1438534863bd52f44641b5c290cfd
akhil3417 <akhilpratapsingh3417@gmail.com>
2023-07-18 05:49:20 +0530

    fix(dired): enable only if module flag active

--- modules/emacs/dired/packages.el
+++ modules/emacs/dired/packages.el
@@ -2,4 +2,5 @@
 ;;; emacs/dired/packages.el
 
 (package! diredfl :pin "62b559e1d6b69834a56a57eb1832ac6ad4d2e5d0")
-(package! dirvish :pin "11a3bec247b9e4742f171434e948895b3dc730ed")
+(when (modulep! +dirvish)
+  (package! dirvish :pin "11a3bec247b9e4742f171434e948895b3dc730ed"))
